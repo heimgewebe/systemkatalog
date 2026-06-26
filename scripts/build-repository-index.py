a729316732bef758996c11a622a53152de6f22d0
Alexander Mohr <alexdermohr@gmail.com>
2026-06-26 13:35:24 +0200

    refactor: simplify inventory entrypoint imports

--- scripts/build-repository-index.py
+++ scripts/build-repository-index.py
@@ -1,41 +1,16 @@
 #!/usr/bin/env python3
 """Stable entrypoint for the Cabinet repository inventory generator."""
 
-try:
-    from scripts.repository_inventory import (
-        InventoryError,
-        _atomic_write,
-        _open_parent_directory,
-        _open_root_directory,
-        _reference_path_components,
-        _split_table_cells,
-        main,
-        os,
-        parse_reference,
-        read_worktree_reference,
-    )
-except ModuleNotFoundError:
-    from repository_inventory import (
-        InventoryError,
-        _atomic_write,
-        _open_parent_directory,
-        _open_root_directory,
-        _reference_path_components,
-        _split_table_cells,
-        main,
-        os,
-        parse_reference,
-        read_worktree_reference,
-    )
-
-
-class _Implementation:
-    pass
-
-
-_implementation = _Implementation()
-_implementation._open_root_directory = _open_root_directory
-_implementation._open_parent_directory = _open_parent_directory
+from repository_inventory import (
+    InventoryError,
+    _atomic_write,
+    _reference_path_components,
+    _split_table_cells,
+    main,
+    os,
+    parse_reference,
+    read_worktree_reference,
+)
 
 
 if __name__ == "__main__":
